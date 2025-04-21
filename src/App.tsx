dbd5bf05a06bd27afd34bf9b7c60713e725e55d7
Abdur Rahaman <rashedhasan967@gmail.com>
2025-04-21 10:37:32 +0600

    installed chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Button colorScheme="blue">Submit</Button>
+    </>
+  );
+}
+
+export default App;
